82e7cec3578ce91d90a86956079fb5d86fadf850
Rachel Zhou <sz287@cornell.edu>
2021-03-29 22:10:22 -0400

    changed player

--- player.mli
+++ player.mli
@@ -1,5 +1,9 @@
 type t
 
+(** The type of a trade. An association list where the key is the player
+    and the value is the list of resources they want to trade away. *)
+type tr = (t * Resource.t list) list
+
 (* TODO: exceptions with 'of' for more info *)
 exception UnknownBuilding
 
@@ -14,5 +18,7 @@ val trade_to_bank : t -> Board.trade_in -> t
 (** [trade_to_player tr] returns a new tuple of players whose lists of
     resources have been updated after a trade has been completed. Raises
     [InvalidTrade] if one or more players has insufficient resources for
-    the desired trade. *)
-val trade_to_player : t -> t -> Resource.t -> Resource.t -> t * t
+    the desired trade. Ex. If p1 wants to trade 1 wool and 2 brick for
+    p2's 2 lumber: trade_to_player
+    [(p1,\[Wool, Brick, Brick\]); (p2, \[Lumber, Lumber\])] *)
+val trade_to_player : tr -> t * t
