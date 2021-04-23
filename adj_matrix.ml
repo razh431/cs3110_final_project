9d8e009c8978fa8da6a4ad39741683f6fdfc5c39
Mindy Li <mzl9@cornell.edu>
2021-04-23 18:14:28 -0400

    Added functions to update player resources on dice roll

--- adj_matrix.ml
+++ adj_matrix.ml
@@ -20,7 +20,7 @@ type tile = {
   id : id;
   dice_num : dice_num;
   resource : resource;
-  corner_position : int list;
+  corner_positions : int list;
   robber : robber;
 }
 
@@ -39,10 +39,9 @@ type node =
   | Some of settlement
   | None
 
-  let init_corners = Array.make 54 None
+let init_corners = Array.make 54 None
 
-let resource_from_string input =
-  match input with
+let resource_from_string = function
   | "Wool" -> Wool
   | "wool" -> Wool
   | "Ore" -> Ore
@@ -55,41 +54,75 @@ let resource_from_string input =
   | "wheat" -> Wheat
   | _ -> failwith "resource can't be parsed"
 
-(*make a single tile*)
+(** [tile_from_json] extracts a single tile from a json [json]. *)
 let tile_from_json json =
   {
     id = json |> member "id" |> to_string |> int_of_string;
     dice_num = json |> member "dice num" |> to_string |> int_of_string;
     resource =
       json |> member "resource" |> to_string |> resource_from_string;
-    corner_position = json |> member "corners" |> to_list |> filter_int;
+    corner_positions = json |> member "corners" |> to_list |> filter_int;
     robber = json |> member "robber" |> to_string |> bool_of_string;
   }
 
 let tiles_from_json json =
   json |> member "tiles" |> to_list |> List.map tile_from_json
 
-  let curr_roads = init_road_mtx
+let curr_roads = init_road_mtx
 
-let update_road_mtx
-    (row : int)
-    (column : int)
-    (value : road) =
+let update_road_mtx (row : int) (column : int) (value : road) =
   curr_roads.(row).(column) <- value
 
 let curr_corners = init_corners
 
-let update_corners index (c : node) =
-  curr_corners.(index) <- c
+let update_corners index (c : node) = curr_corners.(index) <- c
 
-
-
-(*if we roll dice, we want a certain number, and get all the tiles ids*)
 let dice_roll_tiles num json =
   List.filter (fun x -> num = x.dice_num) (tiles_from_json json)
 
-(*check if players have a building on any of those corners, and update
-  player's resources accordingly*)
+(*********** New stuff?? START ************)
+
+(** [settlement_on_corner res node] updates the player with the resource
+    [res] associated with the corner [node]. *)
+let settlement_on_node res = function
+  | Some s -> (
+      match s.building with
+      | House -> Player.update_pl_cards s.player_num House res
+      | City -> Player.update_pl_cards s.player_num City res)
+  | None -> ()
+
+(** [pos_to_nodes positions acc] converts a list of corner positions
+    into a list of the corresponding nodes. *)
+let rec pos_to_nodes positions acc =
+  match positions with
+  | [] -> acc
+  | hd :: tl -> pos_to_nodes tl (curr_corners.(hd - 1) :: acc)
+
+(** [distr_res_of_tile tile] distributes the resource on the tile [tile]
+    to all the players with settlements on the adjacent tiles (who share
+    the corner positions). *)
+let rec distr_res_of_tile tile =
+  (* match tile with | [] -> failwith "TODO" | hd :: tl -> let c =
+     curr_corners.(hd - 1) in settlement_on_node res c;
+     distr_res_of_tile tl res *)
+  List.map
+    (fun node -> settlement_on_node tile.resource node)
+    (pos_to_nodes tile.corner_positions [])
+
+(*********** New stuff?? END ************)
+
+(** [res_of_node corner_pos] returns the resource(s) of the tiles that
+    care adjacent to the corner at position [corner_pos]. *)
+let res_of_node corner_pos = failwith "TODO"
+
+(** [distribute_res player_num res building] distributes resources to a
+    player who has a building [building] on a node with resource [res]. *)
+let distribute_res player_num res = function
+  | House -> failwith "TODO"
+  | City -> failwith "TODO"
+
+(** [settlement_on_corner node] updates the player with the resource
+    associated with the corner [node]. *)
 let settlement_on_corner (x : node) =
   match x with
   | Some settlement -> (
@@ -100,19 +133,14 @@ let settlement_on_corner (x : node) =
       | City -> false)
   | None -> failwith "there are no settlements at this corner"
 
-(*look through corner list for each corner, distrubute resources. *)
-let corner_pos_to_node corner_pos = 
-  let curr_corner = get_curr_corner corner_pos 
-
-
+(** For a given (numerical) corner position, update the resources of the
+    player with a settlement there. *)
+let corner_pos_to_node corner_pos =
+  settlement_on_corner curr_corners.(corner_pos)
 
 let corners_rolled num json =
-  let rec update_pl_resource lst acc =
-    match lst with
-    | [] -> acc
-    | h :: t -> 
-      h.corner_position
+  (* let rec update_pl_resource lst acc = match lst with [] -> acc | h
+     :: t -> h.corner_position in
 
-  in
-
-  update_pl_resource (dice_roll_tiles num json) []
+     update_pl_resource (dice_roll_tiles num json) [] *)
+  ()
--- adj_matrix.mli
+++ adj_matrix.mli
@@ -11,24 +11,24 @@ type building =
   | House
   | City
 
-(* Type of a building, used in [corner], which describes the player who
-   owns it and the kind of building it is. *)
+(** Type of a settlement located at a node; describes the player who
+    owns it and the kind of building it is. *)
 type settlement = {
   player_num : int;
   building : building;
 }
 
-(* Type of a corner in the adjacency matrix. [Some of settlement]
-   describes the player and kind of building if there is a building on
-   that corner, otherwise it has type [None]. *)
+(** Type of a corner in the adjacency matrix. [Some settlement]
+    describes the player and kind of building, if there is a building on
+    that corner; otherwise it has type [None]. *)
 type node =
   | Some of settlement
   | None
 
-(* Type of a road in the adjacency matrix. [Some of int] describes a
-   road owned by player with that number. If there is an unoccupied
-   road, then the road is [Some 0]. If there is no connection then the
-   road is [None].*)
+(** Type of a road in the adjacency matrix. [Some n] describes a road
+    owned by player with number [n]. If there is an unoccupied road,
+    then the road is [Some 0]. If there is no connection then the road
+    is [None].*)
 type road =
   | Some of int
   | None
@@ -37,38 +37,43 @@ type tile = {
   id : id;
   dice_num : dice_num;
   resource : resource;
-  corner_position : int list;
+  corner_positions : int list;
   robber : robber;
 }
 
-(* [tiles_from_json j] makes a list of tiles from the json*)
+(** [tiles_from_json j] makes a list of tiles from the json*)
 val tiles_from_json : Yojson.Basic.t -> tile list
 
-(*[resource_from_string s] parses s into a Resource.t type*)
+(** [resource_from_string s] parses s into a Resource.t type*)
 val resource_from_string : string -> Resource.t
 
-(* [init_road_mtx] makes the inital road matrix that needs to be updated
-   throughout the game*)
+(** [init_road_mtx] makes the inital road matrix. *)
 val init_road_mtx : road array array
 
-(* [update_road_mtx r c v] updates the the matrix with value [v] into
-   row [r] and column [c]*)
+(** [curr_roads] is the mutable adjacency matrix of roads. *)
+val curr_roads : road array array
+
+(** [update_road_mtx r c v] updates the roads matrix at row [r] and
+    column [c] with value [v]. *)
 val update_road_mtx : int -> int -> road -> unit
 
 (*corners functions*)
 
-(* [init_corners] makes the initial corners list.*)
+(** [init_corners] makes the initial array of corners. *)
 val init_corners : node array
 
-(* [update_corners i v ] updates the corner array [a] at tile position
-   [i] with corner v*)
+(** [curr_corners] is the mutable array of corners (i.e. nodes). *)
+val curr_corners : node array
+
+(** [update_corners i v] updates the corner array at tile position [i]
+    with node v*)
 val update_corners : int -> node -> unit
 
-(*[dice_roll_tiles num] if we roll dice, we want a certain number, and
-  get all the tiles with that number in a list*)
-val dice_roll_tiles : int -> tile list
+(** [dice_roll_tiles num] returns a list of all the tiles with the dice
+    roll number that corresponds to [num]. *)
+val dice_roll_tiles : int -> Yojson.Basic.t -> tile list
 
-(* [corners_rolled] finds all the corners of those tiles，check if
-   players have a building on any of those corners, and update player's
-   resources accordingly*)
-val corners_rolled : unit
+(** [corners_rolled] finds all the corners of those tiles，check if
+    players have a building on any of those corners, and update player's
+    resources accordingly*)
+val corners_rolled : int -> Yojson.Basic.t -> unit
